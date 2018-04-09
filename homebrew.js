8c52940f0dbd6f913714c7a708af78697f9fa2de
Christopher Brown <io@henrian.com>
2018-04-09 13:21:28 -0500

    Delint via eslint (mostly autofixes)

--- homebrew.js
+++ homebrew.js
@@ -1,64 +1,64 @@
-const child_process = require('child_process');
-const streaming = require('streaming');
-const Splitter = require('streaming/splitter').Splitter;
-const websum = require('websum');
+const child_process = require('child_process')
+const streaming = require('streaming')
+const {Splitter} = require('streaming/splitter')
+const websum = require('websum')
 
 const request = require('request').defaults({
   headers: {
     // libev happens to blocks blank User-Agents (?!); adopt current Chrome's
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
   },
-});
+})
 
 function getFormulas(callback) {
-  const proc = child_process.spawn('brew', ['list']);
-  const stream = proc.stdout.pipe(new Splitter()).setEncoding('utf8');
+  const proc = child_process.spawn('brew', ['list'])
+  const stream = proc.stdout.pipe(new Splitter()).setEncoding('utf8')
   streaming.readToEnd(stream, (err, formula_names) => {
-    if (err) return callback(err);
+    if (err) return callback(err)
 
-    const formulas = formula_names.map(name => ({name}));
+    const formulas = formula_names.map(name => ({name}))
 
-    callback(null, formulas);
-  });
+    callback(null, formulas)
+  })
 }
-exports.getFormulas = getFormulas;
+exports.getFormulas = getFormulas
 
 function getFormulaInfo(formula_name, callback) {
   // See https://github.com/Homebrew/homebrew/wiki/Querying-Brew for --json=v1 info
-  const proc = child_process.spawn('brew', ['info', '--json=v1', formula_name]);
-  proc.stdout.setEncoding('utf8');
+  const proc = child_process.spawn('brew', ['info', '--json=v1', formula_name])
+  proc.stdout.setEncoding('utf8')
   streaming.readToEnd(proc.stdout, (err, chunks) => {
-    if (err) return callback(err);
+    if (err) return callback(err)
 
-    const content = chunks.join('');
-    console.info('Formula info for %s: %s', formula_name, content);
-    let formula;
+    const content = chunks.join('')
+    console.info('Formula info for %s: %s', formula_name, content)
+    let formula
     try {
       // this breaks for old things like gfortran and pil, which don't return JSON for --json=v1
-      const formulas = JSON.parse(content);
-      formula = formulas[0];
+      const formulas = JSON.parse(content)
+      formula = formulas[0]
     }
     catch (exc) {
-      console.error('Error parsing JSON: %s', exc.message);
+      console.error('Error parsing JSON: %s', exc.message)
     }
 
     if (formula) {
-      console.info('Fetching %s homepage: %s', formula_name, formula.homepage);
+      console.info('Fetching %s homepage: %s', formula_name, formula.homepage)
       request.get(formula.homepage, (err, response, body) => {
-        if (err) return callback(err);
+        if (err) return callback(err)
 
         websum.summarizeHtml(body, 25, (err, summary) => {
-          if (err) return callback(err);
+          if (err) return callback(err)
 
-          formula.summary = summary;
+          formula.summary = summary
 
-          callback(err, formula);
-        });
-      });
+          callback(err, formula)
+        })
+      })
     }
     else {
-      callback(err, formula);
+      callback(err, formula)
     }
-  });
+  })
 }
-exports.getFormulaInfo = getFormulaInfo;
+exports.getFormulaInfo = getFormulaInfo
--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const restify = require('restify');
-const homebrew = require('./homebrew');
+const restify = require('restify')
+const homebrew = require('./homebrew')
 
-const app = restify.createServer();
+const app = restify.createServer()
 
 app.get(/\/static\/?.*/, restify.plugins.serveStatic({
   directory: __dirname,
   default: 'index.html',
-}));
+}))
 
 app.get('api/formulas/:name', (req, res, next) => {
   homebrew.getFormulaInfo(req.params.name, (err, formula) => {
-    if (err) return next(err);
+    if (err) return next(err)
     // max-age is in seconds; 86400 = 1 day; 3600 = 1 hour; 900 = 15 minutes
-    res.setHeader('Cache-Control', 'max-age=86400');
-    res.send(formula);
-    next();
-  });
-});
+    res.setHeader('Cache-Control', 'max-age=86400')
+    res.send(formula)
+    next()
+  })
+})
 
 app.get('api/formulas', (req, res, next) => {
   homebrew.getFormulas((err, formulas) => {
-    if (err) return next(err);
-    res.send(formulas);
-    next();
-  });
-});
+    if (err) return next(err)
+    res.send(formulas)
+    next()
+  })
+})
 
 app.get('/', (req, res, next) => {
-  res.redirect('/static/index.html', next);
-});
+  res.redirect('/static/index.html', next)
+})
 
 app.on('listening', function() {
-  const address = this.address();
-  console.info('server listening on http://%s:%d', address.address, address.port);
-});
+  const address = this.address()
+  console.info('server listening on http://%s:%d', address.address, address.port)
+})
 
-module.exports = app;
+module.exports = app
